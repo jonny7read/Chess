a3931008a8cf73546d2abec253624b129d2f0a69
Jonny Read <jonny7read@gmail.com>
2019-05-27 17:16:05 +0100

    Create PieceType.cs

---
+++ PieceType.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CSharp_Shell
+{
+    
+    public enum PieceType
+    {
+        KING,
+        QUEEN,
+        BISHOP,
+        KNIGHT,
+        ROOK,
+        PAWN
+    }
+    
+}
